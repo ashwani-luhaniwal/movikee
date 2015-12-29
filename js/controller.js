3885051e9da0c17aa56d5f2bcf5ce4c9c315ed28
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2015-12-29 18:24:29 +0530

    Uploading all the files for the project

---
+++ js/controller.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var movieApp = angular.module('MovieApp', []);
+movieApp.controller('MovieController', function($scope, $http) {
+	var requestInfo;
+
+	if ($scope.search === undefined) {
+		$scope.search = "Fashion";
+		fetch();
+	}
+
+	$scope.change = function() {
+		if (requestInfo) {
+			clearTimeout(requestInfo);
+		}
+		requestInfo = setTimeout(fetch, 800);
+	};
+
+	function fetch() {
+		$http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+			.success(function(response) {
+				$scope.details = response;
+			});
+
+		$http.get("http://www.omdbapi.com/?s=" + $scope.search)
+			.success(function(response) {
+				$scope.related = response;
+			});
+	}
+
+	$scope.update = function(movie) {
+		$scope.search = movie.Title;
+		$scope.change();
+	}
+
+	$scope.select = function() {
+		this.setSelectionRange(0, this.value.length);
+	}
+});
